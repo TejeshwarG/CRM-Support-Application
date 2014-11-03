1306bac5b85aa41539accdea3d8f8860263df0c6
TejeshwarG <tejeshwar36@gmail.com>
2014-11-03 22:04:17 +0530

    Create BaseViewModel.cs

---
+++ BaseViewModel.cs
@@ -0,0 +1,20 @@
+using CRM_Application.Helpers;
+using System.ComponentModel;    //INotifyPropertyChanged
+
+namespace CRM_Application.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
